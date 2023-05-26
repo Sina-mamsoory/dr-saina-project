928089e1769783bc4ca538b1e7270585654406f3
sina mansoory <sinaman7979@gmail.com>
2023-05-26 12:27:08 +0330

    making accompanist part (in footer) responsive.

--- src/components/footer/DrSainaAccompanist.jsx
+++ src/components/footer/DrSainaAccompanist.jsx
@@ -8,11 +8,11 @@ const Accompanist = () => {
     return(
         <>
             <Grid container className={styles.container}>
-                <Grid item xs={12} md={8} padding='30px 70px'>
-                    <Typography component='div' variant="h6" fontWeight='bold' color='#065482' textAlign='right'> همراهان دکتر ساینا:</Typography>
+                <Grid item xs={12} md={8} padding='30px 70px' className={styles.comradeContainer}>
+                    <Typography component='div' variant="h6" fontWeight='bold' color='#065482' textAlign='right' className={styles.text}> همراهان دکتر ساینا:</Typography>
                 </Grid>
                 <Grid item xs={12} md={4} padding='30px 0' className={styles.contactContainer}>
-                    <Typography component='div' variant="h6" fontWeight='bold' color='#065482' textAlign='right'> نیاز به راهنمایی دارید؟</Typography>
+                    <Typography component='div' variant="h6" fontWeight='bold' color='#065482' textAlign='right' className={styles.text}> نیاز به راهنمایی دارید؟</Typography>
                     <span className={styles.numberContainer}>021-91005838</span>
                 </Grid>
             </Grid>
